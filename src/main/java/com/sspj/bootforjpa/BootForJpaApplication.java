df2e362102d5bc60f3b6d0ecff94025bf826293f
Ellie <soyeon.shin@inbyu.com>
2021-04-30 17:34:46 +0900

    Initial commit

---
+++ src/main/java/com/sspj/bootforjpa/BootForJpaApplication.java
@@ -0,0 +1,13 @@
+package com.sspj.bootforjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootForJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootForJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sspj/bootforjpa/BootForJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sspj.bootforjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootForJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
